eb5c7beeffd1c8a718a92a17f682e026af991981
MiguelAngel <miguelgiraldo1116@gmail.com>
2025-12-09 19:24:04 +0100

    terminado login y logout del cliente

--- src/Cliente/CallbackUsuarioImpl.java
+++ src/Cliente/CallbackUsuarioImpl.java
@@ -1,7 +1,66 @@
 package Cliente;
 //Miguel Angel Giraldo Polanco :: mgiraldopolanco@gmail.com
 
+import Common.clases.Trino;
 import Common.interfaces.CallbackUsuarioInterface;
+import Common.interfaces.ServicioAutenticacionInterface;
+
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.List;
 
 public class CallbackUsuarioImpl implements CallbackUsuarioInterface {
+    // login y logout
+    public List<Trino> ingresar(String nick) throws RemoteException{
+        try {
+            ServicioAutenticacionInterface autenticacion = (ServicioAutenticacionInterface)
+                    Naming.lookup("rmi://localhost:2002/ServicioAutenticacion/SER1");
+            return autenticacion.ingresar(nick);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public void salir(String nick)throws RemoteException{
+        try {
+            ServicioAutenticacionInterface autenticacion = (ServicioAutenticacionInterface)
+                    Naming.lookup("rmi://localhost:2002/ServicioAutenticacion/SER1");
+            autenticacion.salir(nick);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Metodos user
+    public boolean registrarUsuario(String nombre, String nick, String clave) throws RemoteException{
+        return false;
+    }
+    public boolean bloquearDesbloquearCuenta(String nick, boolean bloquear) throws RemoteException{
+        return false;
+    }
+
+    // Metodos Seguidores
+    public boolean seguirUsuario(String nickSeguidor, String nickSeguido) throws RemoteException{
+        return false;
+    }
+    public boolean dejarDeSeguirUsuario(String nickSeguidor, String nickSeguido) throws RemoteException{
+        return false;
+    }
+
+    // Metodos trinos
+    public void crearTrino (String trino, String nickUsuario) throws RemoteException{
+
+    }
+    public List<Trino> getTrinos(String nicksUsuario) throws RemoteException{
+        return null;
+    }
+    public boolean borrarTrinoTotal(Trino trino) throws RemoteException{
+        return false;
+    }
+
+    // Trinos de los usuarios que sigue el cliente
+    public Trino recibirTrinos ()throws RemoteException{
+        return null;
+    }
 }
--- src/Common/interfaces/CallbackUsuarioInterface.java
+++ src/Common/interfaces/CallbackUsuarioInterface.java
@@ -1,7 +1,31 @@
 package Common.interfaces;
 //Miguel Angel Giraldo Polanco :: mgiraldopolanco@gmail.com
 
+import Common.clases.Trino;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
 public interface CallbackUsuarioInterface extends Remote {
+
+    // login y logout
+    List<Trino> ingresar(String nick) throws RemoteException;
+    void salir(String nick)throws RemoteException;
+
+    // Metodos user
+    boolean registrarUsuario(String nombre, String nick, String clave) throws RemoteException;
+    boolean bloquearDesbloquearCuenta(String nick, boolean bloquear) throws RemoteException;
+
+    // Metodos Seguidores
+    boolean seguirUsuario(String nickSeguidor, String nickSeguido) throws RemoteException;
+    boolean dejarDeSeguirUsuario(String nickSeguidor, String nickSeguido) throws RemoteException;
+
+    // Metodos trinos
+    void crearTrino (String trino, String nickUsuario) throws RemoteException;
+    List<Trino> getTrinos(String nicksUsuario) throws RemoteException;
+    boolean borrarTrinoTotal(Trino trino) throws RemoteException;
+
+    // Trinos de los usuarios que sigue el cliente
+    Trino recibirTrinos ()throws RemoteException;
 }
