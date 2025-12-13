a1a893b311ec7178783b68e1123c96a09f5baa2f
MiguelAngel <miguelgiraldo1116@gmail.com>
2025-12-13 16:57:57 +0100

    Terminado el metodo salir,registrar,ingresar del user

--- src/Cliente/UsuarioUI.java
+++ src/Cliente/UsuarioUI.java
@@ -1,10 +1,16 @@
 package Cliente;
 
 import java.rmi.RemoteException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import Cliente.Buffer;
+import Common.clases.UsuarioData;
 
 public class UsuarioUI{
     private final CallbackUsuarioImpl user;
+    private Set<String> usersOnline = new HashSet<>();
 
     public UsuarioUI(CallbackUsuarioImpl user) {
         this.user = user;
@@ -22,10 +28,10 @@ public class UsuarioUI{
             switch(option){
                 case 1: iniciarSesion(); break;
                 case 2: registrarse(); break;
-                case 3: System.out.println("Saliendo..."); break;
+                case 3: salir(); break;
                 default: System.out.println("Opción inválida");
             }
-        } while(option != 3);
+        } while(option != 0);
     }
 
     private void registrarse() throws RemoteException {
@@ -39,12 +45,17 @@ public class UsuarioUI{
 
     private void iniciarSesion() throws RemoteException {
         String nick = Buffer.readLine("Ingresa tu nick\n");
+        if (!usersOnline.add(nick)){
+            System.out.println("Ya ha iniciado sesion");
+            return;
+        }
+        usersOnline.add(nick);
         var trinos = user.ingresar(nick);
         int option = 0;
         if(trinos != null){
-            System.out.println("Bienvenido " + nick + ". Trinos recientes:");
+            System.out.println("Bienvenido " + nick);
             for(var trino : trinos){
-                System.out.println(trino.GetNickPropietario() + " : "+ trino.GetTrino());
+                System.out.println("Trino reciente: "+trino.GetNickPropietario() + " : "+ trino.GetTrino());
             }
             switch (option){
                 case 1:
@@ -57,6 +68,18 @@ public class UsuarioUI{
             }
         } else {
             System.out.println("Error al iniciar sesión usuario incorrecto");
+        }
+
+    }
+
+    private void salir() throws RemoteException {
+        String nick = Buffer.readLine("Ingresa tu nick\n");
+        if (usersOnline.contains(nick)){
+            user.salir(nick);
+            usersOnline.remove(nick);
+            System.out.println("Adios");
+        }else{
+            System.out.println("El Usuario ingresado no esta online");
         }
     }
 }
