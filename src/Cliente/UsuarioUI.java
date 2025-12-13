f815d4b0f0f99ae4fc5458deebcd712995460065
MiguelAngel <miguelgiraldo1116@gmail.com>
2025-12-13 17:38:19 +0100

    agregado metodo trino y seguir usuarios

--- src/Cliente/UsuarioUI.java
+++ src/Cliente/UsuarioUI.java
@@ -1,16 +1,17 @@
 package Cliente;
 
+import Common.clases.UsuarioData;
+
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import Cliente.Buffer;
-import Common.clases.UsuarioData;
-
 public class UsuarioUI{
     private final CallbackUsuarioImpl user;
     private Set<String> usersOnline = new HashSet<>();
+    private String usuarioActual ;
 
     public UsuarioUI(CallbackUsuarioImpl user) {
         this.user = user;
@@ -21,14 +22,14 @@ public class UsuarioUI{
         do {
             option = Buffer.readInt(
                     "1. Iniciar sesión\n" +
-                            "2. Registrarse\n" +
-                            "3. Salir\n"
+                            "2. Registrarse\n"
             );
 
             switch(option){
-                case 1: iniciarSesion(); break;
-                case 2: registrarse(); break;
-                case 3: salir(); break;
+                case 1: iniciarSesion();
+                    break;
+                case 2: registrarse();
+                    break;
                 default: System.out.println("Opción inválida");
             }
         } while(option != 0);
@@ -51,35 +52,86 @@ public class UsuarioUI{
         }
         usersOnline.add(nick);
         var trinos = user.ingresar(nick);
+        usuarioActual = nick;
         int option = 0;
         if(trinos != null){
             System.out.println("Bienvenido " + nick);
             for(var trino : trinos){
                 System.out.println("Trino reciente: "+trino.GetNickPropietario() + " : "+ trino.GetTrino());
             }
-            switch (option){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default: break;
-            }
+            do {
+                option = Buffer.readInt(
+                        "1. Crear Trino\n" +
+                                "2. Seguir Usuario\n" +
+                                "3. Dejar De Seguir Usuario\n" +
+                                "4. Cerrar Secion\n"
+                );
+
+                switch(option){
+                    case 1: crearTrino();
+                        break;
+                    case 2: seguirUsuario();
+                        break;
+                    case 3: dejarSeguirUsuario();
+                        break;
+                    case 4: salir();
+                        break;
+                    default: System.out.println("Opción inválida");
+                }
+            } while(option != 0);
+
         } else {
             System.out.println("Error al iniciar sesión usuario incorrecto");
         }
 
     }
 
+    private void crearTrino() throws RemoteException {
+        String trino = Buffer.readLine("Escribe un trino\n");
+        user.crearTrino(trino, usuarioActual);
+    }
+
+    private void seguirUsuario() throws RemoteException {
+        String usuario = Buffer.readLine("Escribe el nick del usuario que quieres seguir\n");
+        if (usuarioActual.equals(usuario)){
+            System.out.println("No puedes seguirte a ti mismo");
+            return;
+        }
+        List<UsuarioData>usuarios = user.getUsers();
+        boolean existe = usuarios.stream()
+                .anyMatch(u -> u.getNick().equals(usuario));
+        if(existe) {
+            user.seguirUsuario(usuarioActual, usuario);
+            System.out.println("Haz empezado a seguir a: "+ usuario);
+        }else {
+            System.out.println("El usuario que quieres seguir no existe");
+        }
+    }
+
+    private void dejarSeguirUsuario() throws RemoteException {
+        String usuario = Buffer.readLine("Escribe el nick del usuario que quieres dejar seguir\n");
+        if (usuarioActual.equals(usuario)){
+            System.out.println("No puedes dejar de seguirte a ti mismo");
+            return;
+        }
+        List<UsuarioData>usuarios = user.getUsers();
+        boolean existe = usuarios.stream()
+                .anyMatch(u -> u.getNick().equals(usuario));
+        if(existe) {
+            user.dejarDeSeguirUsuario(usuarioActual, usuario);
+            System.out.println("Haz dejado de seguir a: "+ usuario);
+        }else {
+            System.out.println("El usuario que quieres dejar de seguir no existe");
+        }
+    }
+
     private void salir() throws RemoteException {
-        String nick = Buffer.readLine("Ingresa tu nick\n");
-        if (usersOnline.contains(nick)){
-            user.salir(nick);
-            usersOnline.remove(nick);
+        if (usersOnline.contains(usuarioActual)) {
+            user.salir(usuarioActual);
+            usersOnline.remove(usuarioActual);
+            usuarioActual = null;
             System.out.println("Adios");
-        }else{
-            System.out.println("El Usuario ingresado no esta online");
+            mostrarMenu();
         }
     }
 }
